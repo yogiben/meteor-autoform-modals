31ba1c3dd6df0c7e95c5f082acf1bae77d73ce4f
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2014-10-08 13:44:19 +0100

    New version

--- package.js
+++ package.js
@@ -8,7 +8,10 @@ Package.on_use(function (api) {
 
 api.versionsFrom('METEOR@0.9.0');
 
- api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@2.0.2','raix:handlebar-helpers@0.1.2'], 'client');
+
+ // AAAH!
+ // api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@2.0.2','raix:handlebar-helpers@0.1.2'], 'client');
+ api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@0.17.1','raix:handlebar-helpers@0.1.2'], 'client');
  
  api.add_files('lib/client/modals.html', 'client');
  api.add_files('lib/client/modals.coffee', 'client');
