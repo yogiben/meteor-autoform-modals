db66b5d43d24a0ff2516a257e2aea568243c44c8
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2014-11-13 10:48:22 +0000

    bump

--- package.js
+++ package.js
@@ -6,10 +6,10 @@ Package.describe({
 
 Package.on_use(function (api) {
 
-api.versionsFrom('METEOR@0.9.0');
+api.versionsFrom('METEOR@1.0');
 
 
- api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@3.2.0','raix:handlebar-helpers@0.1.2'], 'client');
+ api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@4.0.2','raix:handlebar-helpers@0.1.2'], 'client');
  
  api.add_files('lib/client/modals.html', 'client');
  api.add_files('lib/client/modals.coffee', 'client');
