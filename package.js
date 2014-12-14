f1b42a0b06f0cf3afd8df5770ec2a2d4df266548
Michal Powaga <michalpowaga13@gmail.com>
2014-12-14 23:41:49 +0000

    Make doc properties accessible for some modal parameters. Fixes #15

--- package.js
+++ package.js
@@ -9,7 +9,17 @@ Package.on_use(function (api) {
 api.versionsFrom('METEOR@1.0');
 
 
- api.use(['jquery','templating','less','session','coffeescript','ui','aldeed:autoform@4.0.2','raix:handlebar-helpers@0.1.2'], 'client');
+ api.use([
+ 	'jquery',
+ 	'templating',
+ 	'less',
+ 	'session',
+ 	'coffeescript',
+ 	'ui',
+ 	'aldeed:autoform@4.0.2',
+ 	'raix:handlebar-helpers@0.1.2',
+ 	'mpowaga:string-template@0.1.0'
+ ], 'client');
  
  api.add_files('lib/client/modals.html', 'client');
  api.add_files('lib/client/modals.coffee', 'client');
