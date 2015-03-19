f52f4d381e959290a6318a0d2b9ea029f296cab9
Ben Jones <benjaminpeterjonesesquire@gmail.com>
2015-03-19 13:18:55 +0000

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Create, update and delete collections with modals",
-  version: "0.3.0",
+  version: "0.3.1",
   git: "https://github.com/yogiben/meteor-autoform-modals"
 });
 
