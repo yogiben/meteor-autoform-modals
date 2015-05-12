28f8c0126900e48a6f57b9e49522d1c87fd2cdbb
Michał Powaga <michalpowaga13@gmail.com>
2015-05-12 22:00:31 +0100

    Fix indentation

--- package.js
+++ package.js
@@ -5,24 +5,21 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@1.0');
 
-api.versionsFrom('METEOR@1.0');
-
-
- api.use([
- 	'jquery',
- 	'templating',
- 	'less',
- 	'session',
- 	'coffeescript',
- 	'ui',
- 	'aldeed:autoform@4.2.2 || 5.1.2',
- 	'raix:handlebar-helpers@0.2.4',
- 	'mpowaga:string-template@0.1.0'
- ], 'client');
+  api.use([
+    'jquery',
+    'templating',
+    'less',
+    'session',
+    'coffeescript',
+    'ui',
+    'aldeed:autoform@4.2.2 || 5.1.2',
+    'raix:handlebar-helpers@0.2.4',
+    'mpowaga:string-template@0.1.0'
+  ], 'client');
  
- api.add_files('lib/client/modals.html', 'client');
- api.add_files('lib/client/modals.coffee', 'client');
- api.add_files('lib/client/modals.less', 'client');
-
+  api.add_files('lib/client/modals.html', 'client');
+  api.add_files('lib/client/modals.coffee', 'client');
+  api.add_files('lib/client/modals.less', 'client');
 });
